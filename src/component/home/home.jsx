4b785db81661bf24320c64de55c66a88e28f86dd
dmmangukiya <dmmangukiya1273@gmail.com>
2023-05-12 11:10:04 +0530

    my first commit reactwebsite

---
+++ src/component/home/home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./home.css";
+
+
+const Home = () => (
+    <section className="home">
+      <div className="content">
+        <h3>Best in <span>Business</span> Products for you</h3>
+        <p>
+          We promise On Time Delivery whether it's  <div className="mon"><p className="soon">summer, winter or monsoon.</p></div>
+        </p>
+        <a href="#products" className="btn">Shop Now</a>
+      </div>
+    </section>
+)
+
+export default Home;
